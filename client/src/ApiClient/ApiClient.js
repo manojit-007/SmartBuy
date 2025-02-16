d6933e33cc640f245595cfcbd5f73379f1d9e218
manojit-007 <159749926+manojit-007@users.noreply.github.com>
2025-02-16 18:33:10 +0530

    2nd commit

--- client/src/ApiClient/ApiClient.js
+++ client/src/ApiClient/ApiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
 //   headers: {
     // Accept: "application/json",
